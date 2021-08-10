756728c19138db3e1cd90b6e2ac3fadade19dcca
Vladimir Zajcev <daniam@internet@ru>
2021-08-11 03:09:12 +0500

    added "delete post" function

--- app.py
+++ app.py
@@ -70,3 +70,13 @@ def edit(id):
             return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
